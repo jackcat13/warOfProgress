9c0a65ef73e9a8341017a421e1782eee6240c631
jackcat13 <christophehenry11@gmail.com>
2025-06-13 14:43:09 +0200

    feat: units selection (one at a time)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use bevy::prelude::*;
 use camera::setup_camera;
-use main_world::setup_villagers;
+use main_world::{setup_villagers, Selected};
 
 mod camera;
 mod main_world;
 
 fn main() {
     App::new()
+        .init_resource::<Selected>()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (setup_camera, setup_villagers))
         .run();
--- src/main_world/mod.rs
+++ src/main_world/mod.rs
@@ -1,21 +1,37 @@
 use bevy::prelude::*;
 use std::fmt::Debug;
+use uuid::Uuid;
 
-#[derive(Component)]
+#[derive(Component, PartialEq)]
 pub struct Villager;
 
+#[derive(Default, Resource, Deref, DerefMut)]
+pub struct Selected {
+    entities: Vec<UnitId>,
+}
+
+#[derive(Component, PartialEq, Clone, Deref, DerefMut)]
+pub struct UnitId(String);
+
 pub fn setup_villagers(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn((
-        Sprite {
-            custom_size: Some(Vec2 { x: 50., y: 70. }),
-            image: asset_server.load("caveman_age/units/units_caveman.png"),
-            ..default()
-        },
-        Villager,
-        Pickable::default(),
-    ))
-        .observe(recollor::<Pointer<Over>>(Color::srgb(0.0, 1.0, 1.0)))
-        .observe(recollor::<Pointer<Out>>(Color::srgb(1.0, 1.0, 1.0)));
+    let villager_asset = asset_server.load("caveman_age/units/units_caveman.png");
+    for index in 0..2 {
+        commands
+            .spawn((
+                Sprite {
+                    custom_size: Some(Vec2 { x: 50., y: 70. }),
+                    image: villager_asset.clone(),
+                    ..default()
+                },
+                Transform::from_xyz(index as f32 * 100., 0., 0.),
+                Villager,
+                UnitId(Uuid::new_v4().to_string()),
+                Pickable::default(),
+            ))
+            .observe(recollor::<Pointer<Over>>(Color::srgb(0.0, 1.0, 1.0)))
+            .observe(recollor::<Pointer<Out>>(Color::srgb(1.0, 1.0, 1.0)))
+            .observe(focus::<Pointer<Pressed>>());
+    }
 }
 
 fn recollor<E: Debug + Clone + Reflect>(color: Color) -> impl Fn(Trigger<E>, Query<&mut Sprite>) {
@@ -27,4 +43,23 @@ fn recollor<E: Debug + Clone + Reflect>(color: Color) -> impl Fn(Trigger<E>, Que
     }
 }
 
-
+fn focus<E>() -> impl Fn(Trigger<E>, Query<(&Sprite, &UnitId)>, ResMut<Selected>)
+where
+    E: Debug + Clone + Reflect,
+{
+    move |ev, query, mut selected| {
+        let Ok((sprite, uuid)) = query.get(ev.target()) else {
+            return;
+        };
+        selected.entities.clear();
+        selected.entities.push(uuid.clone());
+        println!(
+            "Selected entities : {}",
+            selected
+                .entities
+                .iter()
+                .map(|it| it.to_string())
+                .collect::<String>()
+        );
+    }
+}
