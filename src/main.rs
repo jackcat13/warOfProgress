94ca12b582196373602f83093d4e2ea6677eb748
jackcat13 <christophehenry11@gmail.com>
2025-06-16 11:49:57 +0200

    feat: visual circle under selected units

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 use camera::setup_camera;
-use main_world::{setup_villagers, Selected};
+use main_world::{highlight_selected_units, setup_villagers, Selected};
 
 mod camera;
 mod main_world;
@@ -10,6 +10,7 @@ fn main() {
         .init_resource::<Selected>()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (setup_camera, setup_villagers))
+        .add_systems(Update, highlight_selected_units)
         .run();
 }
 
--- src/main_world/mod.rs
+++ src/main_world/mod.rs
@@ -13,9 +13,15 @@ pub struct Selected {
 #[derive(Component, PartialEq, Clone, Deref, DerefMut)]
 pub struct UnitId(String);
 
-pub fn setup_villagers(mut commands: Commands, asset_server: Res<AssetServer>) {
+pub fn setup_villagers(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     let villager_asset = asset_server.load("caveman_age/units/units_caveman.png");
     for index in 0..2 {
+        let uuid = Uuid::new_v4().to_string();
         commands
             .spawn((
                 Sprite {
@@ -25,12 +31,35 @@ pub fn setup_villagers(mut commands: Commands, asset_server: Res<AssetServer>) {
                 },
                 Transform::from_xyz(index as f32 * 100., 0., 0.),
                 Villager,
-                UnitId(Uuid::new_v4().to_string()),
+                UnitId(uuid.clone()),
                 Pickable::default(),
             ))
+            .with_children(|parent| {
+                parent.spawn((
+                    UnitId(uuid),
+                    Mesh2d(meshes.add(Annulus::new(0., 0.))),
+                    MeshMaterial2d(materials.add(Color::srgb(0., 0.5, 0.))),
+                    Transform::from_xyz(-5., -30., 0.)
+                        .with_rotation(Quat::from_rotation_x(90.)),
+                ));
+            })
             .observe(recollor::<Pointer<Over>>(Color::srgb(0.0, 1.0, 1.0)))
             .observe(recollor::<Pointer<Out>>(Color::srgb(1.0, 1.0, 1.0)))
             .observe(focus::<Pointer<Pressed>>());
+    }
+}
+
+pub fn highlight_selected_units(
+    query: Query<(&UnitId, &mut Mesh2d), With<UnitId>>,
+    selected: Res<Selected>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    for (id, mut mesh) in query {
+        *mesh = if selected.contains(id) {
+            Mesh2d(meshes.add(Annulus::new(17., 20.)))
+        } else {
+            Mesh2d(meshes.add(Annulus::new(0., 0.)))
+        }
     }
 }
 
@@ -43,14 +72,17 @@ fn recollor<E: Debug + Clone + Reflect>(color: Color) -> impl Fn(Trigger<E>, Que
     }
 }
 
-fn focus<E>() -> impl Fn(Trigger<E>, Query<(&Sprite, &UnitId)>, ResMut<Selected>)
+fn focus<E>() -> impl Fn(Trigger<E>, Query<(&mut Sprite, &UnitId)>, ResMut<Selected>)
 where
     E: Debug + Clone + Reflect,
 {
-    move |ev, query, mut selected| {
-        let Ok((sprite, uuid)) = query.get(ev.target()) else {
+    move |ev, mut query, mut selected| {
+        let Ok((_sprite, uuid)) = query.get_mut(ev.target()) else {
             return;
         };
+        if selected.entities.contains(uuid) {
+            return;
+        }
         selected.entities.clear();
         selected.entities.push(uuid.clone());
         println!(
