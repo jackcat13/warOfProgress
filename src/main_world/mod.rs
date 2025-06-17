c757dde68e3eeb2afbb18ded2d15a1707e3f62fd
jackcat13 <christophehenry11@gmail.com>
2025-06-17 15:59:02 +0200

    feat: action menu on unit select

--- src/main_world/mod.rs
+++ src/main_world/mod.rs
@@ -1,4 +1,11 @@
-use bevy::{platform::collections::HashMap, prelude::*};
+use bevy::{
+    color::palettes::tailwind::{SKY_700, SLATE_50},
+    ecs::{relationship::RelatedSpawner, spawn::SpawnWith},
+    pbr::material_uses_bindless_resources,
+    platform::collections::HashMap,
+    prelude::*,
+    ui::experimental::UiChildren,
+};
 use std::fmt::Debug;
 use uuid::Uuid;
 
@@ -21,8 +28,13 @@ pub struct SelectChild;
 pub struct UnitId(String);
 
 #[derive(Default, Resource, Deref, DerefMut)]
-pub struct NewPositions{
-    positions: HashMap<UnitId, Vec2>
+pub struct NewPositions {
+    positions: HashMap<UnitId, Vec2>,
+}
+
+#[derive(Debug)]
+enum MenuAction {
+    House,
 }
 
 pub fn setup_villagers(
@@ -30,6 +42,7 @@ pub fn setup_villagers(
     asset_server: Res<AssetServer>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    window: Single<&Window>,
 ) {
     let villager_asset = asset_server.load("caveman_age/units/units_caveman.png");
     for index in 0..2 {
@@ -49,17 +62,55 @@ pub fn setup_villagers(
             .with_children(|parent| {
                 parent.spawn((
                     SelectChild,
-                    UnitId(uuid),
+                    UnitId(uuid.clone()),
                     Mesh2d(meshes.add(Annulus::new(17., 20.))),
                     MeshMaterial2d(materials.add(Color::srgb(0., 0.5, 0.))),
                     Transform::from_xyz(-5., -30., 0.).with_rotation(Quat::from_rotation_x(90.)),
-                    Visibility::Hidden
+                    Visibility::Hidden,
                 ));
             })
             .observe(recollor::<Pointer<Over>>(Color::srgb(0.0, 1.0, 1.0)))
             .observe(recollor::<Pointer<Out>>(Color::srgb(1.0, 1.0, 1.0)))
             .observe(focus::<Pointer<Pressed>>());
+        commands.spawn((
+            SelectChild,
+            UnitId(uuid),
+            Visibility::Hidden,
+            Node {
+                position_type: PositionType::Absolute,
+                bottom: Val::Px(10.),
+                row_gap: Val::Px(10.),
+                ..default()
+            },
+            Children::spawn((SpawnWith(|parent: &mut RelatedSpawner<ChildOf>| {
+                parent
+                    .spawn(button("House"))
+                    .observe(recollor::<Pointer<Over>>(Color::srgb(0.0, 1.0, 1.0)))
+                    .observe(recollor::<Pointer<Out>>(Color::srgb(1.0, 1.0, 1.0)))
+                    .observe(menu_action::<Pointer<Pressed>>(MenuAction::House));
+            }),)),
+        ));
     }
+}
+
+fn menu_action<E: Debug + Clone + Reflect>(menu_action: MenuAction) -> impl Fn(Trigger<E>) {
+    move |ev| {
+        println!("Action {:?}", menu_action);
+    }
+}
+
+fn button<T: Into<String>>(text: T) -> impl Bundle {
+    (
+        Button,
+        BackgroundColor(SKY_700.into()),
+        Node {
+            padding: UiRect::all(Val::Px(5.)),
+            margin: UiRect::all(Val::Px(5.)),
+            width: Val::Px(200.),
+            ..default()
+        },
+        children![(Text::new(text), TextColor(SLATE_50.into()))],
+    )
 }
 
 pub fn highlight_selected_units(
@@ -83,17 +134,23 @@ pub fn check_movement_on_right_click(
     buttons: Res<ButtonInput<MouseButton>>,
     mut new_positions: ResMut<NewPositions>,
 ) {
-    if !buttons.just_released(MouseButton::Right) { return; }
+    if !buttons.just_released(MouseButton::Right) {
+        return;
+    }
     let (camera, camera_transform) = (camera_single.0, camera_single.1);
     for unit_id in query {
-        if !selected.contains(unit_id) { continue; }
+        if !selected.contains(unit_id) {
+            continue;
+        }
         let Some(Ok(world_position)) = window
             .cursor_position()
             .map(|cursor| camera.viewport_to_world_2d(camera_transform, cursor))
         else {
             continue;
         };
-        new_positions.positions.insert(unit_id.clone(), world_position);
+        new_positions
+            .positions
+            .insert(unit_id.clone(), world_position);
     }
 }
 
@@ -104,7 +161,9 @@ pub fn move_units(
 ) {
     let delta = time.delta_secs();
     for (unit_id, mut transform) in query {
-        let Some(target_position) = new_positions.get(unit_id) else { continue; };
+        let Some(target_position) = new_positions.get(unit_id) else {
+            continue;
+        };
         let mut current_position = transform.translation.xy();
         if current_position.x < target_position.x {
             current_position.x += SPEED * delta;
@@ -132,7 +191,7 @@ pub fn move_units(
         }
         transform.translation.x = current_position.x;
         transform.translation.y = current_position.y;
-   }
+    }
 }
 
 fn recollor<E: Debug + Clone + Reflect>(color: Color) -> impl Fn(Trigger<E>, Query<&mut Sprite>) {
